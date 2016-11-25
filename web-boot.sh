7d023a705b624f31a1cbe4bf82bd4ad2c10990c0
Ishfal <ishfal@gmail.com>
2016-11-25 12:57:32 -0800

    Delete web-boot.sh

--- web-boot.sh
+++
@@ -1,3 +0,0 @@
-sed -i 's/^ServerLimit 1/ServerLimit 8/' ~/.heroku/php/etc/apache2/httpd.conf
-sed -i 's/^MaxClients 1/MaxClients 8/' ~/.heroku/php/etc/apache2/httpd.conf
-vendor/bin/heroku-php-apache2
