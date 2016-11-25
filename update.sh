4c31f1cb7069befc3f775b6dc5f1d4de027d9e31
Ishfal <ishfal@gmail.com>
2016-11-25 13:12:52 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # After installation, use to heroku run bash
-# to move to /usr/bin/local/
+# to move to /usr/local/bin/
 #
 cd /app
 php -c /app/.heroku/php/etc/php/php.ini /app/update.php --feeds
